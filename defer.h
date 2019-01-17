6b6a51b625d6d78d4f0355a3432b3a0d1274df59
geniusc <445660334@qq.com>
2019-01-17 09:45:39 +0800

    更新defer 修复无法同时定义多个defer bug

--- defer.h
+++ defer.h
@@ -3,22 +3,27 @@
 namespace sy
 {
 #define defer MAKE_DEFER
-#define MAKE_DEFER defer_action DEFER_VAR
-#define DEFER_VAR _defer_action_ ## __LINE__ ## _
+#define MAKE_DEFER defer_action DEFER_VAR(__LINE__)
+#define DEFER_VAR(line) GENERATE_DEFER_VAR(line)
+#define GENERATE_DEFER_VAR(line) _defer_action_##line##_
 
 struct defer_action
 {
-    defer_action(std::function<void()>&& callback)
-        :m_call(std::forward<std::function<void()>&&>(callback))
+
+    typedef std::function<void()> defer_call;
+    explicit defer_action(defer_call&& callback)
+        :m_call(callback)
     {
-    }
-    ~defer_action()
-    {
-        m_call();
     }
     defer_action(defer_action &rhs) = delete;
     defer_action& operator=(defer_action &rhs) = delete;
     defer_action() = delete;
-    std::function<void()> m_call;
+    defer_call m_call;
+    ~defer_action()
+    {
+        m_call();
+    }
+
 };
+
 }
