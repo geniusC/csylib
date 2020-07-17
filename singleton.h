e73a01a04796c1dbf397c702e348d736a2db5e61
geniusc <sy.chen@live.it>
2020-07-17 22:12:56 +0800

    fix singleton bug under mingw multi-thread

--- singleton.h
+++ singleton.h
@@ -9,27 +9,13 @@ template <typename T>
 class Singleton
 {
 public:
-    static std::shared_ptr<T> instance()
+    static T& instance()
     {
-        std::call_once(ocg, 
-                []() 
-                {
-                m_instance = std::shared_ptr<T>(new T(), [](T *p){delete p;});
-                });
+        static T m_instance;
         return m_instance;
     }
 protected:
-    Singleton(){}
-    virtual ~Singleton(){}
-private:
-    static std::once_flag ocg;
-    static std::shared_ptr<T> m_instance;
+    Singleton() = default;
 };
-template<typename T>
-std::once_flag Singleton<T>::ocg;
-
-template<typename T>
-std::shared_ptr<T> Singleton<T>::m_instance;
-#define DECLARE_SINGLETON(T) friend class Singleton<T>;
 } //namespace sy
 
